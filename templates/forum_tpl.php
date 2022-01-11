ee0e5c5b231345e618460fd6c86973c8df9dbbe0
japmartins <joana.alexandra00@gmail.com>
2022-01-11 17:40:45 +0000

    organizations style

--- templates/forum_tpl.php
+++ templates/forum_tpl.php
@@ -1,5 +1,6 @@
 <html>   
     <?php
+    /* start_session() */
     include('header_tpl.php');
     ?>
     
@@ -20,6 +21,18 @@
         
      </div>
     </body>
+    <?php
+    /* require('init.php')
+    $post_content = $_POST ['']
+    function insertPost()
+    {   
+        global $dbh;
+        $stmt = $dbh->prepare('INSERT INTO Post(text_, user) VALUES (?, ?)');
+        $stmt->execute(array($post_content, $_SESSION ["username"]));}
     
+        try{insertUser($cc_number, $password, $name, $phone_number, $email, $health_number, $date_birth, $address, $doctor);
+    } 
+    catch(PDOException $e) {echo $e;} */
+?>  
 
 </html>
--- templates/organizations_tpl.php
+++ templates/organizations_tpl.php
@@ -13,54 +13,52 @@
         <h2 class = "page_title" > Support </h2>
     </div>
     <div>
-        <ul>
-            <li class = "">
-                <h3>Support Lines</h3> 
-                <ul class = >
-                    <li>
-                        <h4><a href="https://www.sosvozamiga.org/">SOS Voz Amiga</a></h4>
-                        <p>Working hours: 15h30 - 00h30</p>
-                        <p>Contacts: 213 544 546 | 912 802 669 | 963 524 660</p>
-                    </li>
-                    <li>
-                        <h4><a href="https://www.voades.pt/quem-somos">Vozes Amigas de Esperança de Portugal</a></h4>
-                        <p>Working hours: 16:00 - 22:00</p>
-                        <p>Contacts: 222 030 707</p>
-                    </li>
-                    <li>
-                        <h4><a href="http://www.telefone-amizade.pt/site/">Telefone da Amizade</a></h4>
-                        <p>Working hours: 16:00 - 23:00</p>
-                        <p>Contacts: 222 080 707</p>
-                    </li>
-                    <li>
-                        <h4><a href="https://www.vozdeapoio.pt/">Voz de Apoio</a></h4>
-                        <p>Working hours: 21h00 - 24h00</p>
-                        <p>Contacts: 225 506 070</p>
-                        <p>For more information press </p>
-                    </li>
-                </ul>
-            </li>
-            <li class = "">
-                <h3>Support Organizations</h3> 
-                <ul class = >
-                    <li>
-                        <h4><a href="http://www.fnerdm.pt/">FNERDM</a></h4>
-                        <p>Tel: 939 564 509</p>
-                        <p>E-mail: geral@fnerdm.pt</p>
-                    </li>
-                    <li>
-                        <h4><a href="https://www.encontrarse.pt/">Encontrar+se</a></h4>
-                        <p>Contacts: 935 592 507 | 220 101 417</p>
-                        <p>E-mail: geral@encontrarse.pt</p>
-                    </li>
-                    <li>
-                        <h4><a href="https://www.adeb.pt/">Telefone da Amizade</a></h4>
-                        <p>Contacts:218 540 740 | 218 540 744 | 218 540 745</p>
-                        <p>E-mail: adeb@adeb.pt</p>
-                    </li>
-                </ul>
-            </li>
-        </ul>
+        <fieldset>
+            <legend>Support Lines</legend>
+            <ul class = >
+                <li>
+                    <h4><a class="supportLinks" href="https://www.sosvozamiga.org/">SOS Voz Amiga</a></h4>
+                    <p>Working hours: 15h30 - 00h30</p>
+                    <p>Contacts: 213 544 546 | 912 802 669 | 963 524 660</p>
+                </li>
+                <li>
+                    <h4><a class="supportLinks" href="https://www.voades.pt/quem-somos">Vozes Amigas de Esperança de Portugal</a></h4>
+                    <p>Working hours: 16:00 - 22:00</p>
+                    <p>Contacts: 222 030 707</p>
+                </li>
+                <li>
+                    <h4><a class="supportLinks" href="http://www.telefone-amizade.pt/site/">Telefone da Amizade</a></h4>
+                    <p>Working hours: 16:00 - 23:00</p>
+                    <p>Contacts: 222 080 707</p>
+                </li>
+                <li>
+                    <h4><a class="supportLinks" href="https://www.vozdeapoio.pt/">Voz de Apoio</a></h4>
+                    <p>Working hours: 21h00 - 24h00</p>
+                    <p>Contacts: 225 506 070</p>
+                    <p>For more information press </p>
+                </li>
+            </ul>           
+        </fieldset>
+        <fieldset  id = "supportLines">
+            <legend>Suport Organizations</legend>
+            <ul class = >
+                <li>
+                    <h4><a class="supportLinks" href="http://www.fnerdm.pt/">FNERDM</a></h4>
+                    <p>Tel: 939 564 509</p>
+                    <p>E-mail: geral@fnerdm.pt</p>
+                </li>
+                <li>
+                    <h4><a class="supportLinks" href="https://www.encontrarse.pt/">Encontrar+se</a></h4>
+                    <p>Contacts: 935 592 507 | 220 101 417</p>
+                    <p>E-mail: geral@encontrarse.pt</p>
+                </li>
+                <li>
+                    <h4><a class="supportLinks" href="https://www.adeb.pt/">Telefone da Amizade</a></h4>
+                    <p>Contacts:218 540 740 | 218 540 744 | 218 540 745</p>
+                    <p>E-mail: adeb@adeb.pt</p>
+                </li>
+            </ul>
+        </fieldset>
     </div>
 
 </html>    
