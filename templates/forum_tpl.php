bcd0f0c75ea064073694d8d57bbbfc5e5e7d0c5c
japmartins <joana.alexandra00@gmail.com>
2022-01-11 15:23:07 +0000

    Forum + Organizations
    Adicionei o css que o sousa tinha ao header (ainda tem de ser mudado)

---
+++ templates/forum_tpl.php
@@ -0,0 +1,25 @@
+<html>   
+    <?php
+    include('header_tpl.php');
+    ?>
+    
+    <body>
+        <div class = "navbar">   
+            <a href = "organizations_tpl.php">Support</a>
+            <a href = "forum_tpl.php">Foruns</a>
+            <a href = "../database/homepage.html" class = "left">Homepage</a>
+        </div>  
+
+        <div class= "">
+            <h1> Forum </h1>
+
+            <form action="">
+            <input type="text" value = "Write something" name = "post">
+            <input type="submit" value = "Post">           
+            </form>
+        
+     </div>
+    </body>
+    
+
+</html>
